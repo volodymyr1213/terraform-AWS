38f4dcd034c20b80e9f6461269f840ca52816a56
Volodymyr Khomenko <45836705+volodymyr1213@users.noreply.github.com>
2019-02-24 17:19:28 -0600

    Create instances.tf

---
+++ instances.tf
@@ -0,0 +1,8 @@
+resource "aws_instance"    "example" {
+  ami = "ami-0fad7378adf284ce0"
+  instance_type = "t2.micro"
+  key_name = "test_key"
+  associate_public_ip_address = "true"
+  count = 5
+  
+} 
